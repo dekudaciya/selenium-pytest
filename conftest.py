e1b8fd6dd85f36c0f95e4d844d4a38c2c55087e8
Смирнов Василий Васильевич <vsmirnov@msk.termika.ru>
2021-09-10 15:07:43 +0300

    Разработаны тесты для задания

---
+++ conftest.py
@@ -0,0 +1,19 @@
+﻿import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+	parser.addoption('--language', action='store', default=None, help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+	language_name = request.config.getoption("language")
+	options = Options()
+	options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+	browser = webdriver.Chrome(options=options)
+	#browser = webdriver.Chrome(r"D:\chromedriver\chromedriver.exe",options=options)
+	yield browser
+	# этот код выполнится после завершения теста
+	print("\nquit browser..")
+	browser.quit()
+#
---
+++ test_items.py
@@ -0,0 +1,14 @@
+﻿import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time 
+
+def test_see_button_add_to_basket(browser):
+	
+	link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+	browser.get(link)
+	#time.sleep(30)
+	assert browser.find_element_by_css_selector(".btn-add-to-basket"), "No button 'Add to basket' on page!"
+#
